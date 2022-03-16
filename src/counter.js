174485f8420cf1e7651ffbf880953e03b641498e
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-03-16 16:44:41 -0300

    minutes and seconds working!

--- src/counter.js
+++ src/counter.js
@@ -1,38 +1,38 @@
-const counter = document.querySelector("#counter");
-
+const counterHTML = document.querySelector("#counter");
 const timeString = localStorage.getItem("Time");
-const timeArray = timeString.split(":");
 
-const countTime = new Date().setHours(timeArray[0], timeArray[1], 0);
-const TIME_IN_SECONDS = timeArray[0] * 60 * 60 + timeArray[1] * 60;
+function timeToAdd(stringTime) {
+  const timeArray = stringTime.split(":");
+  const hoursInMiliseconds = timeArray[0] * 60 * 60 * 1000;
+  const minutesInMiliseconds = timeArray[1] * 60 * 1000;
+  const TIME_IN_MILISECONDS = hoursInMiliseconds + minutesInMiliseconds;
+  return TIME_IN_MILISECONDS;
+}
 
-const time = (interval) => {
-  const distance = new Date(countTime - now);
-  const hours = String(distance.getHours()).padStart(2, "0");
-  const minutes = String(distance.getMinutes()).padStart(2, "0");
-  const seconds = String(distance.getSeconds()).padStart(2, "0");
+const actualTime = Date.now();
+const finalTimeDateObj = new Date(actualTime + timeToAdd(timeString));
+const finalTime = finalTimeDateObj.getTime();
+
+const time = (myInterval) => {
+  const timeNow = Date.now();
+  const diference = finalTime - timeNow;
+  let displayTimeArray = msToTime(diference);
+  console.log(diference);
+  const hours = String(displayTimeArray[0]).padStart(2, "0");
+  const minutes = String(displayTimeArray[1]).padStart(2, "0");
+  const seconds = String(displayTimeArray[2]).padStart(2, "0");
   if (hours == "00" && minutes == "00" && seconds == "00") {
     clearInterval(myInterval);
   }
-  counter.innerText = `${hours}:${minutes}:${seconds}`;
-  countTime;
+  counterHTML.innerText = `${hours}:${minutes}:${seconds}`;
 };
 
+function msToTime(millis) {
+  var hours = Math.floor(millis / 360000000);
+  var minutes = Math.floor(millis / 1000 / 60) % 60;
+  var seconds = Math.floor(millis / 1000) % 60;
+  console.log(hours, minutes, seconds);
+  return [hours, minutes, seconds];
+}
+
 const myInterval = setInterval(time, 1000);
-
-/*
-function time(TIME_IN_SECONDS) {
-  const clock = new Date(TIME_IN_SECONDS * 1000);
-  console.log(clock);
-  return clock.toLocaleTimeString("pt-br", {
-    timeZone: "UTC",
-  });
-}
-
-function initialClock() {
-  timer = setInterval(function () {
-    seconds--;
-    clock.innerHTML = time(seconds);
-  }, 1000);
-}
-*/
