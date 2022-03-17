3a9466953b3f1079b72f0b1e2cd773a2de248674
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-03-17 18:59:30 -0300

    fixed clearInterval

--- src/counter.js
+++ src/counter.js
@@ -13,16 +13,15 @@ const actualTime = Date.now();
 const finalTimeDateObj = new Date(actualTime + timeToAdd(timeString));
 const finalTime = finalTimeDateObj.getTime();
 
-const time = (myInterval) => {
+const time = () => {
   const timeNow = Date.now();
   const diference = finalTime - timeNow;
   let displayTimeArray = msToTime(diference);
-  console.log(diference);
   const hours = String(displayTimeArray[0]).padStart(2, "0");
   const minutes = String(displayTimeArray[1]).padStart(2, "0");
   const seconds = String(displayTimeArray[2]).padStart(2, "0");
   if (hours == "00" && minutes == "00" && seconds == "00") {
-    clearInterval(myInterval);
+    clearInterval(myTimerInterval);
   }
   counterHTML.innerText = `${hours}:${minutes}:${seconds}`;
 };
@@ -35,4 +34,4 @@ function msToTime(millis) {
   return [h, m, s];
 }
 
-var myInterval = setInterval(time, 1000);
+const myTimerInterval = setInterval(time, 1000);
