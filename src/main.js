e39e764fb9fc1fbf948f1cb58517e422ca53cb88
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-03-21 15:25:50 -0300

    added error message when adding 00:00 as a value

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 const timeInput = document.querySelector(".timeInput");
 const startButton = document.querySelector("startButton");
-
+const error = document.querySelector("#error");
 window.addEventListener("keydown", keyPress);
 
 function keyPress({ key }) {
@@ -12,7 +12,9 @@ function keyPress({ key }) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log(timeInput.value);
-  if (timeInput.value !== "") {
+  if (timeInput.value === "00:00") {
+    error.className = "error";
+  } else if (timeInput.value !== "") {
     localStorage.setItem("Time", timeInput.value);
     window.location.href = "./pages/counter.html";
   }
